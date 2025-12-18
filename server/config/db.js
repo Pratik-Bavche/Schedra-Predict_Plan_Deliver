84736e396433ad9239b390cd8b11be5cd1efd3e3
Pratik <pratikbavche2005@gmail.com>
2025-12-18 22:46:00 +0530

    db updated

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 5000, // Fail after 5s if can't connect
+            socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
+        });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
