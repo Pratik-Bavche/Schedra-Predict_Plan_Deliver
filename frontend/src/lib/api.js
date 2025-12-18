08f153f925ffc1c563b713460a2662c2f1ba5b44
Pratik <pratikbavche2005@gmail.com>
2025-12-18 23:15:06 +0530

    url update

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://schedra-predict-plan-deliver-server.vercel.app/" || "http://localhost:5000/api";
+const BASE_URL = "https://schedra-predict-plan-deliver-server.vercel.app/api" || "http://localhost:5000/api";
 
 export const api = {
     get: async (endpoint) => {
